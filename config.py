8eadec0ff9a2477d76039c716de0088cc2c455cc
terraswapper <spamboite@protonmail.com>
2021-07-10 10:23:27 +0200

    v1.1.3

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import base64
 
 
 class Config:
-    VERSION = "1.1.2"
+    VERSION = "1.1.3"
     _log = logging.getLogger("anchor_borrow")
     formatter = logging.Formatter(
         "%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
