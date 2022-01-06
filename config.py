605105f36a79ac4484da957feb11d7a0afd162f2
terraswapper <spamboite@protonmail.com>
2022-01-06 23:34:13 +0100

    fix version number

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import base64
 
 
 class Config:
-    VERSION = "2.0.3"
+    VERSION = "2.0.6"
     _log = logging.getLogger("anchor_borrow")
     formatter = logging.Formatter(
         "%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
